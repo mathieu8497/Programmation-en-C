e40df6abe4f868d2a09a4b781af15077161eb4ad
Mathieu Poirel <mathieu8497@gmail.com>
2023-09-18 08:53:55 +0200

    Fini les cercles

---
+++ ProgC-master/TP1/src/cercle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main(){
+    float rayon = 3.8;
+    float aire = M_PI*rayon*rayon;
+    float perimetre = 2*M_PI*rayon;
+    printf("Le périmètre est %f\n",perimetre);
+    printf("L'aire est %f\n",aire);
+    return 0;
+}
