fab5a77ff8b3944d5cabc51b0724c041b0d46733
Mathieu Poirel <mathieu8497@gmail.com>
2023-09-18 10:57:59 +0200

    Exo 6

---
+++ ProgC-master/TP1/src/boucles.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int a = 0;
+    int b = 0;
+    int compteur = 8;
+    for (a=0; a < compteur;a++){
+        printf("*\n");
+        for ()
+        //if (a>=3){
+        //    printf("#*\n")
+        }
+    return 0;
+}
