d3502d32c5f14e1f0b62a2d75a2632fd4346c35a
Emma Tricquet <emma.tricquet@cpe.fr>
2023-09-18 09:46:49 +0200

    Exercice sizeof types

--- ProgC-master/TP1/src/sizeof_types.c
+++ ProgC-master/TP1/src/sizeof_types.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
 int main(){
+    sizeof(char);
+    printf("sizeof (char) is %lu\n", sizeof (char));
+
+    sizeof(short);
+    printf("sizeof (short) is %lu\n", sizeof (short));
+
+    sizeof(int);
+    printf("sizeof (int) is %lu\n", sizeof (int));
+
+    sizeof(long int);
+    printf("sizeof (long int) is %lu\n", sizeof (long int));
+
+    sizeof(long long int);
+    printf("sizeof (long long int) is %lu\n", sizeof (long long int));
+
+    sizeof(float);
+    printf("sizeof (float) is %lu\n", sizeof (float));
+
+    sizeof(double);
+    printf("sizeof (double) is %lu\n", sizeof (double));
+
+    sizeof(long double);
+    printf("sizeof (long double) is %lu\n", sizeof (long double));
     
     return 0;
 }
