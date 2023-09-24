e2a00eb1152b2ebb2bde6875d27cadf67fa65828
Emma Tricquet <emma.tricquet@cpe.fr>
2023-09-24 23:56:01 +0200

    Essai

--- ProgC-master/TP1/src/conditions.c
+++ ProgC-master/TP1/src/conditions.c
@@ -2,11 +2,22 @@
 
 int main(){
     int x = 1001;
+    int a = 0;
     // int a = 0;
     //     for (a=0, a<x,a++){
     //         if (a%2==0 && a%15==0){
     //             printf("Ce sont les chiffres %i qui respectent la condition Sont divisibles par 2 et 15 simultanément",a);
     //         }
     //     }
+
+    for (a=0; a < x;a++){
+    //while (){}
+    }
+    
+    //for (){
+    //}
+
+    //for (){
+    //}
     return 0;
 }
