b435c3c085b2e2acd07fa013ca4f62bc236e47ed
Emma Tricquet <emma.tricquet@cpe.fr>
2023-09-25 11:35:26 +0200

    exo 2.4

--- ProgC-master/TP2/src/chaine.c
+++ ProgC-master/TP2/src/chaine.c
@@ -2,7 +2,7 @@
 
 int main(){
     char e[] = "Emma&Mathieu";
-    char a[] = "xxxxxxxxxxxx";
+    char a[] = "Florian&Hugo";
     int cpt = 0;
     while (e[cpt]!='\0'){
         a[cpt] = e[cpt];//parcourir la chaîne source caractère par caractère et les copier dans la chaîne de destination
@@ -10,5 +10,25 @@ int main(){
     }
     printf("%i\n",cpt);
     printf("%s\n",a);
+
+    //concaténation
+    int cpt2 = 0;
+    char f[] = "Florian&Hugo";
+    char concat[]="--------------------------";
+   while (concat[cpt2]!='\0'){
+    if (cpt2<cpt){
+        concat[cpt2] = e[cpt2];
+
+    }
+    else{
+        //printf("%i\n", cpt2-cpt);
+        //printf("%i\n", cpt2);
+        //printf("%i\n", cpt);
+        concat[cpt2]= f[cpt2-cpt];
+    }
+    printf("%s\n",concat);
+    cpt2++;
+
+   }
     return 0;
 }
