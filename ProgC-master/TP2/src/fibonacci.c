ae20df7d7fab5c3a36e86c825bad3caf990efdf1
Mathieu Poirel <mathieu8497@gmail.com>
2023-09-25 09:21:36 +0200

    2.3 Correct

---
+++ ProgC-master/TP2/src/fibonacci.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int un_2 = 0;
+    int un_1= 1;
+    int un =0 ;
+    int n = 7;
+    int x = 0;
+    for (x=0;x<n;x++){
+        un_2 = un_1;
+        un_1 = un;
+        printf("%i\n", un);
+        un = un_1 + un_2;
+    }
+    
+    return 0;
+}
