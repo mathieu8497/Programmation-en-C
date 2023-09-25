09dd5bb991c91cac4827b11143ad9e0eebab2102
Mathieu Poirel <mathieu8497@gmail.com>
2023-09-25 09:49:49 +0200

    2.4

--- ProgC-master/TP2/src/chaine.c
+++ ProgC-master/TP2/src/chaine.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    char e = 'Emma';
-    
+    char e[] = "Emma&Mathieu";
+    char i[] = "";
+    int cpt = 0;
+    while (e[cpt]!='\0'){
+        cpt++;//regarder le nombre de caractère dans la chaine
+        i[cpt] = e[cpt]
+
+    }
+    printf("%i\n",cpt);
     return 0;
 }
