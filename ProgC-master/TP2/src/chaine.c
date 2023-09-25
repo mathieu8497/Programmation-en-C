7fc82c8a315687c3341c18a0203f8c380cef0fbc
Mathieu Poirel <mathieu8497@gmail.com>
2023-09-25 10:23:20 +0200

    finito le 2.5

--- ProgC-master/TP2/src/chaine.c
+++ ProgC-master/TP2/src/chaine.c
@@ -2,13 +2,13 @@
 
 int main(){
     char e[] = "Emma&Mathieu";
-    char i[] = "";
+    char a[] = "xxxxxxxxxxxx";
     int cpt = 0;
     while (e[cpt]!='\0'){
+        a[cpt] = e[cpt];//parcourir la chaîne source caractère par caractère et les copier dans la chaîne de destination
         cpt++;//regarder le nombre de caractère dans la chaine
-        i[cpt] = e[cpt]
-
     }
     printf("%i\n",cpt);
+    printf("%s\n",a);
     return 0;
 }
