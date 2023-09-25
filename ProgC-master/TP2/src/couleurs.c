e1ed0c8445b925f860c23931e3060387e97775ed
Mathieu Poirel <mathieu8497@gmail.com>
2023-09-25 23:53:47 +0200

    Finito les couleurs 2.7

--- ProgC-master/TP2/src/couleurs.c
+++ ProgC-master/TP2/src/couleurs.c
@@ -5,10 +5,10 @@
 int main(){
     int cpt = 0;
     struct couleur {
-    char Rouge[50];
-    char Vert[50];
-    char Bleu[50];
-    char Alpha[50];
+    char Rouge[50]; //= "0xef";
+    char Vert[50]; //= "0x78";
+    char Bleu[50]; //= "0x12";
+    char Alpha[50]; //= "0xff";
     };
 
     struct couleur couleur_rgba[135];
@@ -37,6 +37,31 @@ int main(){
     strcpy(couleur_rgba[4].Vert, "03");
     strcpy(couleur_rgba[4].Bleu, "52");
     strcpy(couleur_rgba[4].Alpha, "59");
+
+    strcpy(couleur_rgba[5].Rouge, "88");
+    strcpy(couleur_rgba[5].Vert, "07");
+    strcpy(couleur_rgba[5].Bleu, "26");
+    strcpy(couleur_rgba[5].Alpha, "14");
+
+    strcpy(couleur_rgba[6].Rouge, "78");
+    strcpy(couleur_rgba[6].Vert, "65");
+    strcpy(couleur_rgba[6].Bleu, "08");
+    strcpy(couleur_rgba[6].Alpha, "03");
+
+    strcpy(couleur_rgba[7].Rouge, "85");
+    strcpy(couleur_rgba[7].Vert, "55");
+    strcpy(couleur_rgba[7].Bleu, "58");
+    strcpy(couleur_rgba[7].Alpha, "85");
+
+    strcpy(couleur_rgba[8].Rouge, "75");
+    strcpy(couleur_rgba[8].Vert, "15");
+    strcpy(couleur_rgba[8].Bleu, "255");
+    strcpy(couleur_rgba[8].Alpha, "24");
+
+    strcpy(couleur_rgba[9].Rouge, "251");
+    strcpy(couleur_rgba[9].Vert, "86");
+    strcpy(couleur_rgba[9].Bleu, "152");
+    strcpy(couleur_rgba[9].Alpha, "159");
 
     for (cpt=0;cpt<10;cpt++){
         printf("Couleur :%i\n",cpt);
