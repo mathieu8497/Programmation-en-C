85b276132e91229cfbbe75043fe5d28a1b2215b1
Emma Tricquet <emma.tricquet@cpe.fr>
2023-09-25 11:58:53 +0200

    Exo 2.4 FINI

--- ProgC-master/TP2/src/chaine.c
+++ ProgC-master/TP2/src/chaine.c
@@ -1,34 +1,23 @@
 #include <stdio.h>
 
 int main(){
-    char e[] = "Emma&Mathieu";
-    char a[] = "Florian&Hugo";
+    char e[30] = "Emma&Mathieu";
+    char a[30] = "Florian&Hugo";
     int cpt = 0;
     while (e[cpt]!='\0'){
         a[cpt] = e[cpt];//parcourir la chaîne source caractère par caractère et les copier dans la chaîne de destination
         cpt++;//regarder le nombre de caractère dans la chaine
     }
-    printf("%i\n",cpt);
-    printf("%s\n",a);
+    printf("Le nombre de caractères dans la chaine -e- vaut : %i\n",cpt);
+    printf("La chaine -a- initialisée à 'Florian&Hugo' est désormais : %s\n",a);
 
     //concaténation
-    int cpt2 = 0;
-    char f[] = "Florian&Hugo";
-    char concat[]="--------------------------";
-   while (concat[cpt2]!='\0'){
-    if (cpt2<cpt){
-        concat[cpt2] = e[cpt2];
-
+    char f[30]="Florian&Hugo";
+    int i = 0; //boucler pour la 2e chaine de caractère
+    while(f[i]!='\0'){
+    e[cpt+i]=f[i];
+    i++;
     }
-    else{
-        //printf("%i\n", cpt2-cpt);
-        //printf("%i\n", cpt2);
-        //printf("%i\n", cpt);
-        concat[cpt2]= f[cpt2-cpt];
-    }
-    printf("%s\n",concat);
-    cpt2++;
-
-   }
+   printf("La concaténation dans la chaine -e- donne le résulat suivant : %s\n",e);
     return 0;
 }
