357fc6ba12083489ea7bb8c3c874b370404e7191
Mathieu Poirel <mathieu8497@gmail.com>
2023-10-02 10:14:57 +0200

    Exo 3.4 finito

---
+++ ProgC-master/TP3/src/chercher.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    int tab[100];
+    int i = 0;
+    int valeur_Utilisateur; 
+    int randomValue;
+    srand(time(NULL));
+    for (i = 0;i<=100;i++){
+        int randomValue = rand() % 1000;
+        tab[i] = randomValue;
+        printf("%d\n", tab[i]);
+    }
+    printf("Entrez l'entier que vous souhaitez chercher : \n");
+    scanf("%d", &valeur_Utilisateur);
+    int valeurComparaison;
+    for (i = 0;i<=100;i++){
+        valeurComparaison = tab[i];
+        if (valeurComparaison == valeur_Utilisateur){
+            printf("Résultat : entier présent\n");
+            return 0;
+        }
+    }
+    printf("Résultat : entier absent\n");   
+    return 0;
+}
