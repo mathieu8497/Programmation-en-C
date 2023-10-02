a629a239a316cea56a7aeed4142a789179efa7c9
Mathieu Poirel <mathieu8497@gmail.com>
2023-10-02 08:19:28 +0200

    Exo 3.1

---
+++ ProgC-master/TP3/src/sizeof.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+    sizeof(int);
+    printf("La taille de int est : %lu octets\n", sizeof (int));
+
+    sizeof(int);
+    printf("La taille de int est : %p octets\n", sizeof (int));
+
+    sizeof(int);
+    printf("La taille de int est : %p octets\n", sizeof (int));
+
+    sizeof(char);
+    printf("La taille de char est : %p octets\n", sizeof (char));
+
+    sizeof(char);
+    printf("La taille de char est : %p octets\n", sizeof (char));
+
+    sizeof(char);
+    printf("La taille de char est : %p octets\n", sizeof (char));
+    
+    sizeof(float);
+    printf("La taille de float est : %p octets\n", sizeof (float));
+
+    sizeof(float);
+    printf("La taille de float est : %p octets\n", sizeof (float));
+
+    sizeof(float);
+    printf("La taille de float est : %p octets\n", sizeof (float));
+
+    return 0;
+}
