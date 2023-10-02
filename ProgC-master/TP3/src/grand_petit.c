78cd46d836361cac23b33d83f37bb2bec29b5d98
Mathieu Poirel <mathieu8497@gmail.com>
2023-10-02 09:02:25 +0200

    Tableau

---
+++ ProgC-master/TP3/src/grand_petit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+    int tab[100];
+    int a;
+    int i = 0;
+    for (i = 0;i<=100;i++){
+        a = rand() % 1000;
+        int x = a;
+        if x
+        tab[i] = a;
+        printf("%d\n", tab[i]);
+    }
+    return 0;
+}
