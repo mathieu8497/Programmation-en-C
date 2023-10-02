23f036561862105ac9a947ddd53e27fa8108a8d3
Mathieu Poirel <mathieu8497@gmail.com>
2023-10-02 09:40:02 +0200

    Exo 3.2 Finito

--- ProgC-master/TP3/src/grand_petit.c
+++ ProgC-master/TP3/src/grand_petit.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <limits.h>
 
 
 int main(){
     int tab[100];
-    int a;
     int i = 0;
+    int int_petit = INT_MAX;
+    int int_grand = INT_MIN;
+    int valeurPrecedente = 0;
+    srand(time(NULL));
     for (i = 0;i<=100;i++){
-        a = rand() % 1000;
-        int x = a;
-        if x
-        tab[i] = a;
+        int randomValue = rand() % 1000;
+        
+        if (randomValue < valeurPrecedente && randomValue < int_petit){
+            int_petit = randomValue;
+        }
+        if (randomValue > valeurPrecedente && randomValue > int_grand){
+            int_grand = randomValue;
+        }
+        tab[i] = randomValue;
         printf("%d\n", tab[i]);
+        valeurPrecedente = randomValue;
     }
+    printf("Le numéro le plus grand est :%i\n", int_grand);
+    printf("Le numéro le plus petit est :%i\n", int_petit);
+
     return 0;
 }
