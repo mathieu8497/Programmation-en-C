6cce22220d73772eccc07a3da5e925a65973d89a
Mathieu Poirel <mathieu8497@gmail.com>
2023-10-03 14:00:44 +0200

    Couleur debut

---
+++ ProgC-master/TP3/src/couleur_compteur.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+int main(){
+    int cpt = 0;
+    int i;
+    struct couleur {
+    unsigned char Rouge;
+    unsigned char Vert;
+    unsigned char Bleu;
+    unsigned char Alpha;
+    };
+
+    struct couleur couleur_rgba[100];
+
+    for (i = 0;i<100;i++){
+    couleur_rgba[i] = (struct couleur){rand()%250, rand()%250, rand()%250, rand()%250};
+    }
+
+    for (cpt=0;cpt<100;cpt++){
+        printf("Couleur :%i\n",cpt);
+        printf("Rouge :%d\n",couleur_rgba[cpt].Rouge);
+        printf("Bleu :%d\n",couleur_rgba[cpt].Vert);
+        printf("Vert :%d\n",couleur_rgba[cpt].Bleu);
+        printf("Alpha :%d\n",couleur_rgba[cpt].Alpha);
+        printf("\n");
+    }
+
+    
+    return 0;
+}
