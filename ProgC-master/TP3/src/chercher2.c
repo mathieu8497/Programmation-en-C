90b66979ee1929c0e1609ad69e33dcb2d7d36067
Emma Tricquet <emma.tricquet@cpe.fr>
2023-10-07 20:46:54 +0200

    exo 3.8

--- ProgC-master/TP3/src/chercher2.c
+++ ProgC-master/TP3/src/chercher2.c
@@ -5,7 +5,7 @@ Auteurs : Mathieu Poirel & Emma Tricquet*/
 #include <stdio.h>
 
 int main(){
-    char phrases[10][200] = {
+    char phrases[10][60] = {
         "Bonjour, comment ça va ?",
         "Le temps est magnifique aujourd'hui.",
         "C'est une belle journée.",
@@ -20,23 +20,30 @@ int main(){
 
     int motTrouve = 0;
     int tailleMax = 10;
+    int verif =0;
     int cpt;
     int carac;
-    char PhraseDonnee = "Les pointeurs en C peuvent être déroutants.";
-
+    char PhraseDonnee[60] = "Les pointeurs en C peuvent être déroutants.";
+    
     for (cpt =0; cpt < tailleMax; cpt++){
-        for (carac= 0; carac< 200; carac++)
+        for (carac = 0; carac <60; carac++)
         {
-            if (PhraseDonnee[carac]==phrases[cpt][carac]){
+            if (PhraseDonnee[carac]==phrases[cpt][carac])
+            {
+                verif++;
                 continue;
             }
-            
+            else
+            {
+                break;
+            } 
         }
-        
+        if (verif==60)
+        {
+            printf("Phrase trouvée : %s", PhraseDonnee);
+            break;
+        }
     
     }
-    
-
-
     return 0;
 }
