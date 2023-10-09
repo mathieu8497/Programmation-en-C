cdfcae08f0bec77b4da285c78bafd9be4916ba88
Mathieu Poirel <mathieu8497@gmail.com>
2023-10-09 09:31:37 +0200

    Main exo 1

--- ProgC-master/TP4/src/main.c
+++ ProgC-master/TP4/src/main.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include "operator.h"
+
+int main()
+{
+    int numero1;
+    int numero2;
+    int resultat
+    char op;
+
+
+    printf("Entrez num1 : \n");
+    scanf("%d", &numero1);
+    printf("Entrez num2 : \n");
+    scanf("%d", &numero2);
+    printf("Entrez l'opérateur (+, -, *, /, %, &, |, ~) : \n");
+    scanf("%s", &op);
+
+    printf("Résultat: %d\n", resultat);
+    return 0;
+}
+
 
