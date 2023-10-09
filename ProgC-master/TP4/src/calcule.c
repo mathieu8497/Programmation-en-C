e09c9e6deb31d542bfa9af4b56e697f65ea977d9
Mathieu Poirel <mathieu8497@gmail.com>
2023-10-09 08:26:39 +0200

    4.1

--- ProgC-master/TP4/src/calcule.c
+++ ProgC-master/TP4/src/calcule.c
@@ -2,12 +2,6 @@
 
 int main() {
 
-   int tableau[100];
-
-   for (int compteur = 0; compteur < sizeof(tableau); compteur++) { //Erreur
-       tableau[compteur] = tableau[compteur] * 2;
-   }
-   
    return (0);
 
 }
