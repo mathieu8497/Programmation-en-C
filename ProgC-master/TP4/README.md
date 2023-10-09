77100a8c0c6e6ea99359f6a4b0a6c97e5744dce7
Emma Tricquet <emma.tricquet@cpe.fr>
2023-10-09 11:49:42 +0200

    Exo 4.4 fini

--- ProgC-master/TP4/README.md
+++ ProgC-master/TP4/README.md
@@ -5,7 +5,7 @@
 # Références
 * https://johnsamuel.info/fr/enseignement/cours/2023/C/cours2.html#slide67
 * https://openclassrooms.com/forum/sujet/signification-argc-argv-79291
-*
+* https://stackoverflow.com/questions/64285928/implicit-declaration-of-function-atoi
 
 # Difficulté
 *
--- ProgC-master/TP4/src/calcule.c
+++ ProgC-master/TP4/src/calcule.c
@@ -1,11 +1,64 @@
 /*Nom du fichier : calcule.c
-Objectif : 
+Objectif :
 Auteurs : Mathieu Poirel & Emma Tricquet*/
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "operator.h"
 
-int main() {
 
-   return (0);
+int main(int argc, char *argv[])
+{
 
+    // saisie protégée (il faudra s'assurer d'un entier)
+    if (argc != 4)
+    {
+        printf("Toutes les informations ne sont pas requises pour effectuer l'opération \n");
+    }
+    else
+    {
+        int num1 = atoi(argv[2]);
+        int num2 = atoi(argv[3]);
+        int resultat;
+
+        switch (*argv[1])
+        {
+        case '+':
+            resultat = addition(num1, num2);
+            break;
+
+        case '-':
+            resultat = soustraction(num1, num2);
+            break;
+
+        case '*':
+            resultat = multiplication(num1, num2);
+            break;
+
+        case '%':
+            resultat = divisionEntiere(num1, num2);
+            break;
+
+        case '/':
+            resultat = division(num1, num2);
+            break;
+
+        case '&':
+            resultat = et(num1, num2);
+            break;
+
+        case '|':
+            resultat = ou(num1, num2);
+            break;
+
+        default:
+            break;
+        }
+
+        printf("%i \n",resultat);
+        
+    }
+
+    return (0);
 }
