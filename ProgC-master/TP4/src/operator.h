98d0d0371ba5e9b3348c5b47014b551fa18b5139
Mathieu Poirel <mathieu8497@gmail.com>
2023-10-09 08:46:36 +0200

    Fichier opérateur

---
+++ ProgC-master/TP4/src/operator.h
@@ -0,0 +1,28 @@
+void addition(int a, int b) 
+{ 
+    printf("L'addition donne =%d\n", a + b); 
+} 
+void multiplication(int a, int b) 
+{ 
+    printf("La multiplication donne=%d\n", a * b); 
+}
+void soustraction(int a, int b) 
+{ 
+    printf("La soustraction donne=%d\n", a - b); 
+} 
+void divisionEntiere(int a, int b) 
+{ 
+    printf("La division entière donne=%d\n", a % b); 
+} 
+void division(int a, int b) 
+{ 
+    printf("La division donne=%d\n", a / b); 
+} 
+void et(int a, int b) 
+{ 
+    printf("Le ET Logique donne=%d\n", a & b); 
+} 
+void ou(int a, int b) 
+{ 
+    printf("Le OU Logique donne=%d\n", a | b); 
+} 
