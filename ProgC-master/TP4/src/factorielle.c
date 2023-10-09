5920655f054f9cd9b26f9a63bacd4c4b2509330c
Emma Tricquet <emma.tricquet@cpe.fr>
2023-10-09 12:07:10 +0200

    Exo 4.5 fini

---
+++ ProgC-master/TP4/src/factorielle.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// Définition de la fonction factorielle
+int factorielle(int num) {
+  if (num == 0) {
+    printf("fact(0): 1\n");
+    return 1;
+  } else {
+    int valeur = num * factorielle(num - 1);
+    printf("fact(%d): %d\n", num, valeur);
+    return (valeur);
+  }
+}
+
+int main() {
+  int n;
+  
+  // Testez la fonction factorielle avec différentes valeurs d'entiers naturels
+  int test;
+
+  test = factorielle(3);
+  printf("%i",test);
+
+  return 0;
+}
