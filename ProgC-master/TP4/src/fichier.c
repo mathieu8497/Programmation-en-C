8ea181b0b1232b82bad55d9f180ad73a10e9d904
Mathieu Poirel <mathieu8497@gmail.com>
2023-10-17 22:56:45 +0200

    format fichier

--- ProgC-master/TP4/src/fichier.c
+++ ProgC-master/TP4/src/fichier.c
@@ -6,37 +6,36 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-    // Fonction lire fichier
+// Fonction lire fichier
 
-void lire_fichier(char* nom_de_fichier)
+void lire_fichier(char *nom_de_fichier)
+{
+    char contenu[1000];
+    printf("Contenu du fichier : %s\n", nom_de_fichier);
+    int fd = open(nom_de_fichier, O_RDONLY);
+    int taille = read(fd, contenu, 1000);
+    for (int compteur = 0; compteur < taille; compteur++)
     {
-        char contenu[1000];
-        printf("Contenu du fichier : %s\n", nom_de_fichier);
-        int fd = open(nom_de_fichier, O_RDONLY);
-        int taille = read(fd, contenu, 1000);
-        for (int compteur = 0; compteur < taille; compteur++)
-        {
-            printf("%c", contenu[compteur]);
-        }
-        close(fd);
+        printf("%c", contenu[compteur]);
     }
+    close(fd);
+}
 
-    // Fonction ecrire fichier
+// Fonction ecrire fichier
 
-    void ecrire_dans_fichier(char* nom_de_fichier, char* message)
-    {
-        int fd = open(nom_de_fichier, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
-        write(fd, message, sizeof(message));
-        printf("Le message a été écrit dans le fichier %s\n", nom_de_fichier);
-    }
-    
+void ecrire_dans_fichier(char *nom_de_fichier, char *message)
+{
+    int fd = open(nom_de_fichier, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+    write(fd, message, sizeof(message));
+    printf("Le message a été écrit dans le fichier %s\n", nom_de_fichier);
+}
+
 // Programme principal
 int main()
 {
     int choix;
     char nom_de_fichier[50];
     char message[1000];
-
 
     printf("Que souhaitez-vous faire ? : \n 1. Lire un fichier \n 2. Écrire dans un fichier \n");
     printf("Votre choix : \n");
@@ -45,7 +44,7 @@ int main()
     if (choix == 1)
     {
         printf("Entrez le nom du fichier à lire : \n");
-        scanf(" %49s",nom_de_fichier);
+        scanf(" %49s", nom_de_fichier);
         lire_fichier(nom_de_fichier);
     }
     else if (choix == 2)
